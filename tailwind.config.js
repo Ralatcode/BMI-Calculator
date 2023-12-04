b5c712d61a0c850c10caec6fd68144f4e0880125
RalatPC <ralatrado@gmail.com>
2023-12-04 19:23:13 +0100

    screen and color config setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "sm": "592px",
+        "md": "768px",
+        "lg": "1280px"
+      },
+      colors: {
+        "transparent": "transparent",
+        "blue": "hsl(227, 92%, 58%)",
+        "gun-metal": "hsl(215, 31%, 21%)",
+        "dark-electric-blue": "hsl(215, 17%, 45%)",
+        "borders": "hsl(200, 24%, 88%)",
+        "pure-white": "hsl(0, 0%, 100%)",
+        "hero-from": "hsla(184, 95%, 92%, 0%)",
+        "hero-to": "hsl(216, 95%, 92%)",
+        "form-from": "hsl(227, 92%, 59%)",
+        "form-to": "hsl(227, 92%, 67%)",
+        "advice-from": "hsla(216, 95%, 92%, 30%)",
+        "advice-to": "hsla(184, 95%, 92%, 20%)",
+        "red": "#ef4444",
+      }
+    },
   },
   plugins: [],
 }
