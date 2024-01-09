b052e52b0e38920bbb01838674451d46f5a2d79e
RalatPC <ralatrado@gmail.com>
2024-01-09 20:53:00 +0100

    script to return input cm and kg

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let radios = document.querySelectorAll("input[type='radio'");
+let radios = document.querySelectorAll("input[type='radio']");
 const metric = document.querySelector('#metric');
 const imperial = document.querySelector('#imperial');
 radios = Array.from(radios)
@@ -10,11 +10,36 @@ radios.forEach((radio) => {
                 if (eachRadio === radios[1]) {
                     metric.style.display = 'none';
                     imperial.style.display = 'grid';
+                    getKg();
                 } else if (eachRadio === radios[0]) {
                     imperial.style.display = 'none';
                     metric.style.display = 'grid';
+                    getCm();
                 }
             }
         }
     })
 })
+
+
+
+function getCm() {
+    const cm = document.querySelector("input[name='centimeter']");
+    cm.addEventListener('input', () => {
+        console.log(cm.value)
+        return cm.value;
+    })
+}
+
+function getKg() {
+    const kg = document.querySelector("input[name='kilogram']");
+    kg.addEventListener('input', () => {
+        console.log(kg.value)
+        return kg.value;
+    })
+}
+
+
+
+getCm();
+getKg();
